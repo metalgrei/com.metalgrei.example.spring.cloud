6e3d4b98d2134157df5ac9df5241187725ed0e2e
metalgrei <isaac.etsia@gmail.com>
2015-01-24 13:31:21 +0100

    TERCERA PRUEBA

--- com.metalgrei.example.spring.cloud/src/main/java/com/metalgrei/example/spring/cloud/App.java
+++ com.metalgrei.example.spring.cloud/src/main/java/com/metalgrei/example/spring/cloud/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.print("HOLA");
 		System.out.print("ESTO ES UNA PRUEBA DE GIT");
+		System.out.print("PARECE QUE SE COMO FUNCIONA");
 	}
 
 }
