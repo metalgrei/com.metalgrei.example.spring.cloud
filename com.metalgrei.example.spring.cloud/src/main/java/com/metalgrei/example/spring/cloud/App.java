e91bc9999083489c327f81ea6b657f93c3ec4436
metalgrei <isaac.etsia@gmail.com>
2015-01-24 17:54:33 +0100

    PRUEBA JAIME 1

--- com.metalgrei.example.spring.cloud/src/main/java/com/metalgrei/example/spring/cloud/App.java
+++ com.metalgrei.example.spring.cloud/src/main/java/com/metalgrei/example/spring/cloud/App.java
@@ -8,6 +8,7 @@ public class App {
 		System.out.print("PARECE QUE SE COMO FUNCIONA");
 		System.out.print("Y YO TAMBIEN");
 		System.out.print("LO HE TOCADO YO");
+		System.out.print("PRUEBA JAIME 1");
 		System.out.print("TENGO QUE LEER FILOSOFIA GIT");
 	}
 
